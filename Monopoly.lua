dc60ba54f2b7cd9634339ae52f167382d5cdc36d
KabanBunya <82792960+KabanBunya@users.noreply.github.com>
2021-07-18 12:43:28 +0300

    Add files via upload

--- Monopoly.lua
+++ Monopoly.lua
@@ -5433,7 +5433,7 @@ function autoupdate(json_url, prefix, url)
                     elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
                       sampAddChatMessage(('[Mono Tools]{FFFFFF} Скрипт успешно обновлён.'), 0x046D63)
 					  sampAddChatMessage(('[Mono Tools]{FFFFFF} Ознакомиться со всеми обновлениями вы сможете в Меню скрипта - помощь - обновления.'), 0x046D63)
-					  win_state['tup'].v = not win_state['tup'].v
+					  testupdate()
                       goupdatestatus = true
                       lua_thread.create(function() wait(500) thisScript():reload() end)
                     end
